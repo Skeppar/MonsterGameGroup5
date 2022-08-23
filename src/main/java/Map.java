60291ae6521693b7f2799302a58ee4b2809afbca
Magnus <magnusgronhult@gmail.com>
2022-08-23 13:30:50 +0200

    map push, compile error

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class Map {
 
+    private char block = '\u2588';
+    private ArrayList<Position> positions;
 
+    public Map() {
+        this.positions = new ArrayList<>();
+    }
+
+    public void map1(Terminal terminal) {
+        for (int a = 6; a < 16; a++) { // a = distance from right a < length. Next (a, star from top)
+            positions.add(new Position(a, 10));
+
+
+            terminal.setCursorPosition(a, 10);
+            terminal.putCharacter(block);
+        }
+
+    }
 
 }
